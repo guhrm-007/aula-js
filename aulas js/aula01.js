05d2ee71129c70ff15f51a0eb26d728214922f79
GuhRM <quintino124@icloud.com>
2025-10-08 07:52:01 -0300

    aula js

---
+++ aulas js/aula01.js
@@ -0,0 +1,5 @@
+
+console.log("Esta linha está comentada")
+console.log("Alô Mundo")
+console.log("Fim")
+
---
+++ aulas js/aula03.js
@@ -0,0 +1,15 @@
+"use script"
+
+/* let é mais seguro de utilizar e conseguimos trocar a variavel a qualquer momento
+e também conseguimos trocar o tipo de valor */
+
+let nome="Bruno"  
+nome="CFB cursos"
+nome=10
+
+const curso="Javascript"  // const é uma não variavel e não conseguimos alterar o valor
+
+
+curso="HTML"
+
+console.log(curso)
